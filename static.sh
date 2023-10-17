81f28904e2e8c0797bbf23a989b571b6c78a852d
Mikk Margus Möll <mikk.margus.moll@eesti.ee>
2023-10-18 01:01:43 +0300

    github ci/cd stuff

--- static.sh
+++ static.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 apk add alpine-sdk go libpcap-dev
-git clone https://github.com/monoidic/porttest
-go build -C porttest/server -tags netgo -trimpath -ldflags="-s -w '-extldflags=-static -lpcap'"
-go build -C porttest/client -tags netgo -trimpath -ldflags="-s -w"
+go build -C src/server -tags netgo -trimpath -buildmode=pie -ldflags="-s -w '-extldflags=-static -lpcap'"
+go build -C src/client -tags netgo -trimpath -buildmode=pie -ldflags="-s -w"
