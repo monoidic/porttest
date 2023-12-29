1adb5bf341f64ec833c640acca991ba7cac004c3
Mikk Margus Möll <mikk@you.die.ifyou.work>
2023-12-30 00:14:06 +0200

    alpine workaround

--- static.sh
+++ static.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 apk add alpine-sdk go libpcap-dev
+
+## workaround for alpine breaking Go with this error:
+## go: cannot find GOROOT directory: 'go' binary is trimmed and GOROOT is not set
+export GOROOT=/usr/lib/go
+
 go build -C src/server -tags netgo -trimpath -buildmode=pie -ldflags="-s -w -linkmode=external '-extldflags=-static-pie -lpcap'"
 go build -C src/client -tags netgo -trimpath -buildmode=pie -ldflags="-s -w -linkmode=external '-extldflags=-static-pie'"
