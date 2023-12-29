76f37b749e4330d3d0e953fd208990e407cad924
Mikk Margus Möll <mikk@you.die.ifyou.work>
2023-12-29 23:42:53 +0200

    fix broken static pie?

--- static.sh
+++ static.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 apk add alpine-sdk go libpcap-dev
-go build -C src/server -tags netgo -trimpath -buildmode=pie -ldflags="-s -w '-extldflags=-static -lpcap'"
-go build -C src/client -tags netgo -trimpath -buildmode=pie -ldflags="-s -w"
+go build -C src/server -tags netgo -trimpath -buildmode=pie -ldflags="-s -w -linkmode=external '-extldflags=-static-pie -lpcap'"
+go build -C src/client -tags netgo -trimpath -buildmode=pie -ldflags="-s -w -linkmode=external '-extldflags=-static-pie'"
