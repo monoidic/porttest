1d0ba7f9ea59fb4c8638203f3f0beabc7e7a4486
Mikk Margus Möll <mikk@you.die.ifyou.work>
2025-10-03 21:51:09 +0300

    guess you need procfs now

--- static.sh
+++ static.sh
@@ -2,13 +2,9 @@
 
 # lol
 # lmao
-sed -i 's/v3.20/edge/' /etc/apk/repositories
+#sed -i 's/v3.22/edge/' /etc/apk/repositories
 
 apk add alpine-sdk go libpcap-dev
-
-## workaround for alpine breaking Go with this error:
-## go: cannot find GOROOT directory: 'go' binary is trimmed and GOROOT is not set
-export GOROOT=/usr/lib/go
 
 go build -C src/server -tags netgo -trimpath -buildmode=pie -ldflags="-s -w -linkmode=external '-extldflags=-static-pie -lpcap'"
 go build -C src/client -tags netgo -trimpath -buildmode=pie -ldflags="-s -w -linkmode=external '-extldflags=-static-pie'"
